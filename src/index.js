e5e34b7fcaafeb7e9dc86c799e691e7f90140625
Chris Nielsen <chnl@dr.dk>
2020-10-14 13:26:15 +0200

    chore(release): v1.1.0

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 // Some change...
+
+
 async function run() {
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
 }
 
+// Another change
 run().catch(console.error);
 
